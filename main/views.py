515e289d317f7059563559d39dd51b236e02df6f
unknown <otabecktursunov@gmail.com>
2024-12-09 18:27:28 +0500

    shukrullo aka uchun

--- main/views.py
+++ main/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def test_view(request):
+    return HttpResponse('Salom, bu Django Views orqali jo\'natildi!')
+
