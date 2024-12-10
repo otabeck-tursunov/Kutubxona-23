f176a33833dec680fce3ff42a88e53d61f979fe1
unknown <otabecktursunov@gmail.com>
2024-12-10 11:28:11 +0500

    last commit

--- core/urls.py
+++ core/urls.py
@@ -6,4 +6,8 @@ from main.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('test/', test_view),
+    path('', home_view),
+    path('talabalar/', talabalar_view),
+    path('talabalar/<int:talaba_id>/', talaba_details_view),
+    path('kitoblar/', kitoblar_view),
 ]
--- main/views.py
+++ main/views.py
@@ -1,7 +1,46 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
+from .models import *
 
 
 def test_view(request):
-    return HttpResponse('Salom, bu Django Views orqali jo\'natildi!')
+    talabalar = Talaba.objects.all()
+    temp = ""
+    for i, talaba in enumerate(talabalar):
+        temp += f"<p>{i + 1}. {talaba.ism} [{talaba.kurs}-kurs]</p>\n"
+    return HttpResponse(
+        f'''
+        <h3>Talabalar ro'yxati</h3>
+        {temp}
+        '''
+    )
 
+
+def home_view(request):
+    return render(request, 'home.html')
+
+
+def talabalar_view(request):
+    talabalar = Talaba.objects.all()
+    context = {
+        'talabalar': talabalar
+    }
+    return render(request, 'talabalar.html', context)
+
+
+def talaba_details_view(request, talaba_id):
+    talaba = Talaba.objects.get(id=talaba_id)
+    context = {
+        'talaba': talaba
+    }
+    return render(request, 'talaba_details.html', context)
+
+
+
+def kitoblar_view(request):
+    kitoblar = Kitob.objects.all()
+    context = {
+        'kitoblar': kitoblar
+    }
+    return render(request, 'kitoblar.html', context)
