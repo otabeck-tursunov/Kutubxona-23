787e7ecfdb2d852485cd14841859f42fcb176957
Otabek Tursunov <otabecktursunov@gmail.com>
2024-12-13 19:48:51 +0500

    commit

--- core/urls.py
+++ core/urls.py
@@ -7,7 +7,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('test/', test_view),
     path('', home_view),
-    path('talabalar/', talabalar_view),
+    path('talabalar/', talabalar_view, name='talabalar'),
     path('talabalar/<int:talaba_id>/', talaba_details_view),
-    path('kitoblar/', kitoblar_view),
+    path('kitoblar/', kitoblar_view, name='kitoblar'),
+    path("kitob-qo'shish/", kitob_qoshish_view, name='kitob-qoshish'),
+    path('mualliflar/', mualliflar_view, name='mualliflar'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from datetime import datetime
 from .models import *
@@ -22,9 +22,25 @@ def home_view(request):
 
 
 def talabalar_view(request):
+    if request.method == "POST":
+        Talaba.objects.create(
+            ism=request.POST.get('ism'),
+            kurs=request.POST.get('kurs'),
+            guruh=request.POST.get('guruh'),
+            telefon_raqam=request.POST.get('telefon_raqam'),
+            yosh=request.POST.get('yosh'),
+            kitob_soni=request.POST.get('kitob_soni'),
+        )
+        return redirect('talabalar')
     talabalar = Talaba.objects.all()
+
+    search = request.GET.get('search')
+    if search is not None:
+        talabalar = talabalar.filter(ism__contains=search)
+
     context = {
-        'talabalar': talabalar
+        'talabalar': talabalar,
+        'search': search
     }
     return render(request, 'talabalar.html', context)
 
@@ -37,10 +53,49 @@ def talaba_details_view(request, talaba_id):
     return render(request, 'talaba_details.html', context)
 
 
-
 def kitoblar_view(request):
     kitoblar = Kitob.objects.all()
     context = {
         'kitoblar': kitoblar
     }
     return render(request, 'kitoblar.html', context)
+
+
+def kitob_qoshish_view(request):
+    if request.method == "POST":
+        kitob = Kitob.objects.create(
+            nom=request.POST.get('nom'),
+            janr=request.POST.get('janr'),
+            sahifa=request.POST.get('sahifa'),
+            muqova=request.POST.get('muqova')
+        )
+        muallif = get_object_or_404(Muallif, id=request.POST.get('muallif_id'))
+        kitob.muallif.add(muallif)
+        kitob.save()
+        return redirect('kitoblar')
+    context = {
+        'mualliflar': Muallif.objects.all()
+    }
+    return render(request, 'kitob_qoshish.html', context)
+
+
+def mualliflar_view(request):
+    if request.method == "POST":
+        if request.POST.get('tirik') == "on":
+            tirik = True
+        else:
+            tirik = False
+        Muallif.objects.create(
+            ism=request.POST.get('ism'),
+            millat=request.POST.get('millat'),
+            jins=request.POST.get('jins'),
+            kitob_soni=request.POST.get('kitob_soni'),
+            t_yil=request.POST.get('t_yil'),
+            tirik=tirik
+        )
+        return redirect('mualliflar')
+    mualliflar = Muallif.objects.all()
+    context = {
+        'mualliflar': mualliflar
+    }
+    return render(request, 'mualliflar.html', context)
